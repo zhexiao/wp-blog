085f0e2881f72b38fdae8d860802ea033ee0a54c
AndyBeginIt <zhexiao@163.com>
2014-12-17 01:14:46 -0500

    add webmaster and sitemap

--- wp-content/themes/zhexiao/header.php
+++ wp-content/themes/zhexiao/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="google-site-verification" content="tfCBws9YRRnNh_m6OON2Me-y_tz1gWpQflINYuQCfGU" />
 	<meta name="description" content="<?=bloginfo('description'); ?>" />
 	<title><?=wp_title( '', false, 'right' );?></title>
 	<?php wp_head(); ?>
