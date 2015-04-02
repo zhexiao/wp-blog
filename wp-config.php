2c18cd2f1fbb341a3902ba7129112b791cba989b
Zhe Xiao <zhexiao@163.com>
2015-04-02 14:41:04 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'zxblog');
+define('DB_NAME', 'wp-blog');
 
 /** MySQL database username */
 define('DB_USER', 'root');
