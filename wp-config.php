96467eb7703856a056aad87cc018182090495581
Zhe Xiao <zhexiao@163.com>
2015-10-20 12:12:26 -0400

    init blog in local

--- wp-config.php
+++ wp-config.php
@@ -22,13 +22,13 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp-blog');
+define('DB_NAME', 'wpblog');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xiaozhe');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
